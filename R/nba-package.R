7988a5f9306233379c1d320359412f5100cee785
Imad Ali <imadmali@gmail.com>
2017-09-13 00:05:12 -0400

    starting pkg description

---
+++ R/nba-package.R
@@ -0,0 +1,14 @@
+#' Accessing the NBA API
+#'
+#' @name nba-package
+#' @aliases nba
+#' @description
+#'
+#' The \pkg{nba} package is designed to make accessing the NBA API
+#' (\link{stat.com.nba}) easier. It provides functions to download, for example,
+#' shot chart data or play-by-play data. Additionally it provides graphical
+#' functions to plot court markings, which can be used in conjunction with the
+#' shot chart data.
+#'
+
+NULL
